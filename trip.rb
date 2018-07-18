769ac465611d19abd570479ec88170cd9381ea49
Hope Zouzas <hmzouzas@gmail.com>
2018-07-18 14:08:04 +0000

    Done.

--- trip.rb
+++ trip.rb
@@ -5,16 +5,24 @@
 puts "Let's plan your trip! Where would you like to stay?"
 
 # Then, set a variable called stay to get the user's input. Make sure to capitilize that input.
-stay = gets.chomp.capitalize  #don't forget to Capitilize the input
+stay = gets.chomp.capitalize  
+#don't forget to Capitilize the input
 
-puts "Where do you want to eats"
-eats = gets.chomp.capitalize
+
+
+
+puts "Where do you want to eat"
+eat = "pizza"
+# gets.chomp.capitalize
+# "pizza"
 
 
 # Do the same thing with `eats` and one last time with `nights`, capturing their text input and storing them to the respective local variable
 puts "How many nights do you want to stay?"
-nights = gets.chomp.capitalize
+nights = "4"
+# gets.chomp.capitalize
+# "4"
 
 # Lastly, puts it all back by interpolating these values in a string.
 
-puts "In your trip you will stay in #{stay} for #{nights} and eats at #{eats}."
+puts "In your trip you will stay in #{stay} for #{nights} and eats at #{eat}."
